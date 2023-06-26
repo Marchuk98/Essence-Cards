ae0de6dc3e38ff10028bfaa97a09c9f558e5c959
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-06-26 05:48:30 +0300

    add polymorphic button

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import { createRoot } from 'react-dom/client'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import { App } from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './styles/index.scss'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/'} element={<App />} />
+        <Route path={'/home'} element={<div>Home</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
