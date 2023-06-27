c8a973660f0a0b06aa36049696e2708afb15d8db
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-06-27 03:49:33 +0300

    add polymorphic typography

---
+++ src/components/ui/typography/typography.tsx
@@ -0,0 +1,33 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import s from './typography.module.scss'
+
+export type TypographyPropsType<T extends ElementType = 'p'> = {
+  as?: T
+  variant:
+    | 'large'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'body_1'
+    | 'body_2'
+    | 'subtitle_1'
+    | 'subtitle_2'
+    | 'caption'
+    | 'overline'
+    | 'link_1'
+    | 'link_2'
+  children?: ReactNode
+  className?: string
+} & ComponentPropsWithoutRef<'p'>
+export const Typography = <T extends ElementType = 'p'>(
+  props: TypographyPropsType<T> & Omit<ComponentPropsWithoutRef<T>, keyof TypographyPropsType<T>>
+) => {
+  const { children, variant, className, as: Component = 'p', ...rest } = props
+
+  return (
+    <Component className={`${s[variant]} ${className ?? ''}`} {...rest}>
+      {children}
+    </Component>
+  )
+}
