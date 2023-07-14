c4bec4eb1a5a67cb5dd813d172f8c6a24822a8a6
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-07-14 15:19:44 +0300

    feat: create PageButton component doing pagination

---
+++ src/components/ui/pagination/primary-pagination-buttons/page-button/page-button.tsx
@@ -0,0 +1,30 @@
+import clsx from 'clsx'
+
+import { Typography } from '../../../typography'
+
+import s from './page-button.module.scss'
+
+export type NavigationButtonPropsType = {
+  onClick: () => void
+  disabled?: boolean
+}
+
+type PageButtonPropsType = NavigationButtonPropsType & {
+  page: number
+  selected: boolean
+}
+
+export const PageButton = ({ onClick, disabled, selected, page }: PageButtonPropsType) => {
+  const classNames = {
+    button: clsx(s.pageButton, selected && s.selected),
+    typography: clsx(selected && s.selectedPageNumber),
+  }
+
+  return (
+    <button onClick={onClick} disabled={selected || disabled} className={classNames.button}>
+      <Typography className={classNames.typography} variant={'body_2'}>
+        {page}
+      </Typography>
+    </button>
+  )
+}
