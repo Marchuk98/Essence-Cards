9a0d2180569e9e6e1f87f3a9b7681ee1c6c67903
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-07-18 23:53:05 +0300

    feat: create table component

---
+++ src/components/ui/table/table.stories.tsx
@@ -0,0 +1,152 @@
+import { useState } from 'react'
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Sort, Table } from './'
+import { CustomCheckbox } from '../checkbox'
+import { Grades } from '../grades'
+
+const data = [
+  {
+    id: '01',
+    title: 'Project A',
+    cardsCount: 10,
+    updated: '2023-07-07',
+    createdBy: 'John Doe',
+  },
+  {
+    id: '02',
+    title: 'Project B',
+    cardsCount: 5,
+    updated: '2023-07-06',
+    createdBy: 'Jane Smith',
+  },
+  {
+    id: '03',
+    title: 'Project C',
+    cardsCount: 8,
+    updated: '2023-07-05',
+    createdBy: 'Alice Johnson',
+  },
+  {
+    id: '04',
+    title: 'Project D',
+    cardsCount: 3,
+    updated: '2023-07-07',
+    createdBy: 'Bob Anderson',
+  },
+  {
+    id: '05',
+    title: 'Project E',
+    cardsCount: 12,
+    updated: '2023-07-04',
+    createdBy: 'Emma Davis',
+  },
+]
+
+const columns = [
+  {
+    key: 'name',
+    title: 'Name',
+    sortable: true,
+  },
+  {
+    key: 'cardsCount',
+    title: 'Cards',
+    sortable: true,
+  },
+  {
+    key: 'updated',
+    title: 'Last Updated',
+    sortable: true,
+  },
+  {
+    key: 'createdBy',
+    title: 'Created by',
+    sortable: true,
+  },
+  {
+    key: 'actions',
+    title: '',
+    sortable: false,
+  },
+]
+
+const meta = {
+  title: 'Components/Table',
+  component: Table.Root,
+  decorators: [
+    Story => (
+      <div style={{ margin: '3em', display: 'flex', justifyContent: 'center' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof Table.Root>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: () => {
+    const [sort, setSort] = useState<Sort>(null)
+
+    return (
+      <Table.Root>
+        <Table.Head columns={columns} sort={sort} onSort={setSort} />
+        <Table.Body>
+          {data.map(row => {
+            return (
+              <Table.Row key={row.id}>
+                <Table.TableCell>{row.title}</Table.TableCell>
+                <Table.TableCell>{row.cardsCount}</Table.TableCell>
+                <Table.TableCell>{row.updated}</Table.TableCell>
+                <Table.TableCell>{row.createdBy}</Table.TableCell>
+                <Table.TableCell>Option...temp</Table.TableCell>
+              </Table.Row>
+            )
+          })}
+        </Table.Body>
+      </Table.Root>
+    )
+  },
+}
+
+export const Head_Cell: Story = {
+  args: {
+    children: <Table.HeadCell key={'name'} title={'Name'} />,
+  },
+}
+
+export const Data_Cell: Story = {
+  args: {
+    children: <Table.TableCell>{'Name'}</Table.TableCell>,
+  },
+}
+
+export const Data_Cell_With_CheckBox_Name: Story = {
+  args: {
+    children: <Table.TableCell>{<CustomCheckbox label={'Name'} />}</Table.TableCell>,
+  },
+}
+
+export const Data_Cell_With_Grade: Story = {
+  args: {
+    children: (
+      <Table.TableCell>
+        <Grades
+          grade={3}
+          onChange={() => {
+            console.log()
+          }}
+        />
+      </Table.TableCell>
+    ),
+  },
+}
+
+export const Data_Cell_With_Checkbox: Story = {
+  args: {
+    children: <Table.TableCell>{<CustomCheckbox />}</Table.TableCell>,
+  },
+}
---
+++ src/components/ui/table/table.tsx
@@ -0,0 +1,134 @@
+import { ComponentProps, ComponentPropsWithoutRef, FC } from 'react'
+import { clsx } from 'clsx'
+
+import { Typography } from '../typography'
+import { ChevronDown } from '../../../images/svg/icons'
+
+import s from './table.module.scss'
+
+type TableProps = { className?: string } & ComponentProps<'table'>
+
+const Root: FC<TableProps> = ({ className, ...rest }) => {
+  const classNames = clsx(s.table, className)
+
+  return <table className={classNames} {...rest} />
+}
+
+export type Sort = {
+  columnKey: string
+  direction: 'asc' | 'desc' | null
+} | null
+type Column = {
+  key: string
+  title: string
+  sortable: boolean
+}
+type HeadProps = Omit<
+  ComponentPropsWithoutRef<'thead'> & {
+    columns: Column[]
+    sort?: Sort
+    onSort?: (sort: Sort) => void
+    className?: string
+  },
+  'children'
+>
+const Head: FC<HeadProps> = ({ columns, sort, onSort, className, ...rest }) => {
+  const handlerSort = (key: string, sortable?: boolean) => {
+    if (!onSort || !sortable) return
+
+    if (key !== sort?.columnKey) {
+      return onSort({ columnKey: key, direction: 'asc' })
+    }
+    if (sort.direction === 'asc') {
+      return onSort({ columnKey: key, direction: 'desc' })
+    }
+
+    onSort(null)
+  }
+
+  return (
+    <thead className={className} {...rest}>
+      <tr>
+        {columns.map(col => {
+          const handler = () => {
+            handlerSort(col.key, col.sortable)
+          }
+
+          return (
+            <Table.HeadCell
+              sort={sort}
+              title={col.title}
+              onClick={handler}
+              key={col.key}
+              columnKey={col.key}
+              sortable={col.sortable}
+            />
+          )
+        })}
+      </tr>
+    </thead>
+  )
+}
+
+type BodyProps = { className?: string } & ComponentProps<'tbody'>
+const Body: FC<BodyProps> = ({ className, ...rest }) => {
+  return <tbody className={className} {...rest} />
+}
+
+type RowProps = { className?: string } & ComponentProps<'tr'>
+const Row: FC<RowProps> = ({ className, ...rest }) => {
+  return <tr className={className} {...rest} />
+}
+
+type HeadCellProps = {
+  minWidth?: number
+  className?: string
+  title?: string
+  columnKey?: string
+  sort?: Sort
+  onClick?: (sortDirection: string) => void
+  sortable?: boolean
+} & ComponentProps<'th'>
+const HeadCell: FC<HeadCellProps> = ({
+  sortable,
+  onClick,
+  sort,
+  columnKey,
+  title,
+  className,
+  ...rest
+}) => {
+  const classNames = {
+    th: clsx(s.headCell, !sortable && s.noSort, className),
+    title: clsx(s.title),
+    icon: clsx(s.sortDscIcon, sort?.direction === 'asc' && s.sortAscIcon),
+  }
+  const showSortIcon = sort?.columnKey === columnKey && sort?.direction
+  const handleClick = () => {
+    if (onClick && columnKey) {
+      onClick(columnKey)
+    }
+  }
+
+  return (
+    <th className={classNames.th} {...rest} onClick={handleClick}>
+      <div className={classNames.title}>
+        <Typography variant={'subtitle_2'}>{title}</Typography>
+        <div className={classNames.icon}>{showSortIcon && <ChevronDown />}</div>
+      </div>
+    </th>
+  )
+}
+
+type TableCellProps = { className?: string } & ComponentProps<'td'>
+const TableCell: FC<TableCellProps> = ({ children, content, className, ...rest }) => {
+  const classNames = clsx(s.tableCell, className)
+
+  return (
+    <td className={classNames} {...rest}>
+      {children}
+    </td>
+  )
+}
+
+export const Table = { TableCell, HeadCell, Row, Body, Head, Root }
