44c0525ad2171f7160f1d5763d4980a4235cd239
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-07-24 22:04:40 +0300

    upd: code refactoring logic modification and avatar component styling

--- src/components/ui/avatar/avatar.stories.tsx
+++ src/components/ui/avatar/avatar.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { UserAvatar } from './avatar.tsx'
+import { UserAvatar } from './'
 
 const meta = {
   title: 'Components/Avatar',
@@ -12,5 +12,10 @@ export default meta
 export type Story = StoryObj<typeof meta>
 
 export const UserAvatar_Demo: Story = {
-  args: {},
+  args: {
+    name: 'story Name',
+    src: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80',
+    width: 46,
+    height: 46,
+  },
 }
--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -1,17 +1,34 @@
-import * as Avatar from '@radix-ui/react-avatar'
+import * as AvatarRadix from '@radix-ui/react-avatar'
+
+import { ReactNode } from 'react'
+import { clsx } from 'clsx'
 
 import s from './avatar.module.scss'
 
-export const UserAvatar = () => {
+export type UserAvatarProps = {
+  src?: string
+  name: string
+  avatarContent?: ReactNode
+  width?: number
+  height?: number
+  className?: string
+}
+
+export const UserAvatar = (props: UserAvatarProps) => {
+  const { name, src, avatarContent, width, height, className } = props
+
+  const classNames = {
+    avatarRoot: clsx(s.avatarRoot, className),
+    avatar: s.avatar,
+    icon: s.icon,
+    fallback: s.fallback,
+  }
+
   return (
-    <div className={s.wrapper}>
-      <Avatar.Root className={s.AvatarRoot}>
-        <Avatar.Image
-          className={s.AvatarImage}
-          src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-          alt="Colm Tuite"
-        />
-      </Avatar.Root>
-    </div>
+    <AvatarRadix.Root className={classNames.avatarRoot} style={{ width, height }}>
+      <AvatarRadix.Image className={classNames.avatar} src={src} alt={name} />
+      {avatarContent && <div className={classNames.icon}>{avatarContent}</div>}
+      <AvatarRadix.Fallback className={classNames.fallback}>{name[0]}</AvatarRadix.Fallback>
+    </AvatarRadix.Root>
   )
 }
