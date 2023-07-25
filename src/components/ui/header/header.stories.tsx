63153028ee014f041be4835eb6111ae69d1c9df3
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-07-25 03:47:28 +0300

    upd: code refactoring logic modification and header component stylings

--- src/components/ui/header/header.stories.tsx
+++ src/components/ui/header/header.stories.tsx
@@ -1,37 +1,58 @@
+import { useState } from 'react'
+
 import { Meta, StoryObj } from '@storybook/react'
 
 import { Header } from './'
-import { LogoCardProject } from '../../../images/svg/icons'
-import { Button } from '../button'
-import { UserAvatar } from '../avatar'
 
 const meta = {
   title: 'Components/Header',
   component: Header,
   tags: ['autodocs'],
+  argTypes: {
+    isAuth: {
+      options: [true, false],
+      control: { type: 'radio' },
+    },
+    name: [],
+    avatar: [''],
+  },
 } satisfies Meta<typeof Header>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Header_Demo: Story = {
+export const Default: Story = {
   args: {
-    children: (
-      <>
-        <LogoCardProject />
-        <Button variant={'primary'}>Sign In</Button>
-      </>
-    ),
+    isAuth: false,
+    name: 'Alexey',
+    avatar: 'https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80',
+    email: 'Gaba00275@yandex.ru',
   },
 }
 
-export const Header_Demo_User: Story = {
+export const Control_Auth_Person: Story = {
   args: {
-    children: (
-      <>
-        <LogoCardProject />
-        <UserAvatar name={'Alexey'} />
-      </>
-    ),
+    isAuth: true,
+    name: 'Alexey',
+    email: 'alexey777@yandex.ru',
+    avatar: 'https://...',
+  },
+
+  render: args => {
+    const [isAuth, setIsAuth] = useState<boolean>(false)
+
+    const handleSignOut = () => {
+      setIsAuth(!isAuth)
+    }
+
+    return (
+      <Header
+        {...args}
+        onSignIn={handleSignOut}
+        onSignOut={handleSignOut}
+        onProfileSelect={() => alert('Redirect to personal-information')}
+        isAuth={isAuth}
+      />
+    )
   },
 }
--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,62 +1,63 @@
-import { LogoCardProject, Logout } from '../../../images/svg/icons'
-
 import { CustomDropdownMenu, DropdownItem, DropdownItemWithIcon } from '../dropdown-menu'
 
-import s from './header.module.scss'
 import { UserAvatar } from '../avatar'
-import { ProfileIcon } from '../../../images/svg/icons/ProfileSvg.tsx'
 import { Typography } from '../typography'
-import { Link } from 'react-router-dom'
 import { Button } from '../button'
+import { LogoCardProject, Logout, Person } from '../../../images/svg/icons'
+
+import s from './header.module.scss'
 
 type HeaderProps = {
   isAuth: boolean
-  userInfo?: {
-    name: string
-    avatar: string
-    email: string
-  }
+  name?: string
+  avatar?: string
+  email?: string
+  onSignIn?: () => void
   onProfileSelect?: () => void
   onSignOut?: () => void
 }
 
 export const Header = (props: HeaderProps) => {
   // TODO:  в процессе разработки логики доработка avatar, onProfileSelect, onSignOut
-  const { isAuth, userInfo, onProfileSelect, onSignOut } = props
+  const { isAuth, name, email, avatar, onProfileSelect, onSignIn, onSignOut } = props
 
   return (
     <header className={s.header}>
-      <Link to={'/'} className={s.logoLink}>
-        <LogoCardProject />
-      </Link>
-      <Typography className={s.userName} variant={'body_2'} as={'label'}>
-        {userInfo?.name}
-      </Typography>
-      {isAuth && (
+      <LogoCardProject />
+      {/*<Link to={'/'} className={s.logoLink}>*/}
+      {/*    */}
+      {/*</Link>*/}
+      {isAuth ? (
         <CustomDropdownMenu
           trigger={
             <button className={s.userMenuTrigger}>
-              <UserAvatar />
+              <Typography className={s.userName} variant={'body_2'} as={'label'}>
+                {name}
+              </Typography>
+              <UserAvatar name={'header Name'} src={avatar} width={36} height={36} />
             </button>
           }
         >
           <DropdownItem>
             <div className={s.userInfoContainer}>
-              <UserAvatar />
+              <UserAvatar name={'dropdown Name'} src={avatar} width={36} height={36} />
               <div className={s.userDetails}>
-                <Typography variant="body_2">{userInfo?.name}</Typography>
+                <Typography variant="body_2">{name}</Typography>
                 <Typography variant="caption" className={s.userEmail}>
-                  {userInfo?.email}
+                  {email}
                 </Typography>
               </div>
             </div>
           </DropdownItem>
 
-          <DropdownItemWithIcon icon={<ProfileIcon />} text="Profile" onSelect={onProfileSelect} />
+          <DropdownItemWithIcon icon={<Person />} text="Profile" onSelect={onProfileSelect} />
           <DropdownItemWithIcon icon={<Logout />} text="Sign out" onSelect={onSignOut} />
         </CustomDropdownMenu>
+      ) : (
+        <Button onClick={onSignIn} variant={'primary'}>
+          <Typography variant={'subtitle_2'}>Sign In</Typography>
+        </Button>
       )}
-      {!isAuth && <Button variant="primary">Sign In</Button>}
     </header>
   )
 }
