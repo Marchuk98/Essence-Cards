74f076fd50858cb6b08913118c657a360b66cbe4
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-07-27 23:46:56 +0300

    fix: updated validation rules and default values in the login form

--- src/common/schemas/use-login-form.ts
+++ src/common/schemas/use-login-form.ts
@@ -5,18 +5,18 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
 const schema = z.object({
-  login: z.string().trim().nonempty('Enter login').min(3, 'Login must be at least 3 character'),
+  email: z.string().trim().email('Invalid email address').nonempty('Enter email'),
   password: z
     .string()
     .trim()
-    .nonempty('Enter login')
-    .min(8, 'Password must be at least 8 character'),
+    .nonempty('Enter password')
+    .min(3, 'Password must be at least 8 character')
+    .max(30, 'Password must be no more 30 character'),
   rememberMe: z.literal<boolean>(true, {
     errorMap: () => {
       return { message: 'You must agree to the terms and condition' }
     },
   }),
-  email: z.string().trim().email('Invalid email address').nonempty('Enter email'),
 })
 
 export type LoginFormInputs = z.infer<typeof schema>
@@ -25,6 +25,11 @@ export const useLoginForm = (onSubmit: SubmitHandler<LoginFormInputs>) => {
   const { handleSubmit, ...rest } = useForm<LoginFormInputs>({
     resolver: zodResolver(schema),
     mode: 'onSubmit',
+    defaultValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
   })
 
   return {
