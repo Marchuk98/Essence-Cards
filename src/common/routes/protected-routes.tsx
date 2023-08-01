fd4e105bdbe9cd7394581be77c7ab4a6d2ff9c80
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-01 23:26:42 +0300

    upd: updating and adding new routes

--- src/common/routes/protected-routes.tsx
+++ src/common/routes/protected-routes.tsx
@@ -1,7 +1,11 @@
-import { useMeQuery } from '../../services/auth/auth-endpoints/auth.api.ts'
+import { useGetMeQuery } from '../../services/auth/auth-endpoints/auth.api.ts'
+import { Navigate, Outlet } from 'react-router-dom'
+import { PATH } from '../constants'
 
 export const ProtectedRoutes = () => {
-  const { data, isLoading } = useMeQuery()
+  const { data, isLoading } = useGetMeQuery()
 
   if (isLoading) return <div>...Loading</div>
+
+  return data ? <Outlet /> : <Navigate to={PATH.LOGIN} />
 }
--- src/common/routes/routes.tsx
+++ src/common/routes/routes.tsx
@@ -1,20 +1,30 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { Layout } from '../../components/layout'
-import { ProtectedRoutes } from './protected-routes'
 import { PATH } from '../constants'
+import { Packs } from '../../services/packs/components/packs/packs.tsx'
 import { Login } from '../../services/auth/components/login/login.tsx'
+import { ProtectedRoutes } from './protected-routes.tsx'
+import { App } from '../../app/App.tsx'
+import { Register } from '../../services/auth/components/register/register.tsx'
+import { PasswordRecovery } from '../../services/auth/components/password-recovery/password-recovery.tsx'
+import { CheckEmail } from '../../services/auth/components/check-email/check-email.tsx'
+import { NewPassword } from '../../services/auth/components/new-password/new-password.tsx'
+import { Profile } from '../../services/profile/components/profile/profile.tsx'
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
+    element: <App />,
     children: [
       {
         element: <ProtectedRoutes />,
         children: [
           {
-            // path: '/',
-            // element: <Decks />,
+            path: PATH.PACKS,
+            element: <Packs />,
+          },
+          {
+            path: PATH.PROFILE,
+            element: <Profile />,
           },
         ],
       },
@@ -22,6 +32,24 @@ export const router = createBrowserRouter([
         path: PATH.LOGIN,
         element: <Login />,
       },
+      {
+        path: PATH.REGISTRATION,
+        element: <Register />,
+      },
+      {
+        path: PATH.PASSWORD_RECOVERY,
+        element: <PasswordRecovery />,
+      },
+      {
+        path: PATH.CHECK_EMAIL,
+        element: <CheckEmail />,
+      },
+      {
+        path: PATH.NEW_PASSWORD,
+        element: <NewPassword />,
+      },
     ],
   },
 ])
+
+
