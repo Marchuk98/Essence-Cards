301154c9218e584da8895dc2fffc33930180a9b2
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-18 20:14:55 +0300

    upd: refactor personal-information component for API integration

--- src/components/auth/personal-information/personal-information.tsx
+++ src/components/auth/personal-information/personal-information.tsx
@@ -1,68 +1,60 @@
-import { Button, Card, Typography, UserAvatar, FileInput } from '../../ui'
+import { Button, Card, FileInput, Typography, UserAvatar } from '../../ui'
 
-import { ProfileFormInput, UseProfileForm } from '../../../common'
-import { useState } from 'react'
+import { ProfileAvatarForm, UseProfileAvatarForm } from '../../../common'
 
 import { Edit, Logout } from '../../../images/svg/icons'
-import { ControlledTextField } from '../../controlled'
+
+import { EditableTextField } from '../../ui/editable-text-field'
+import { useGetMeQuery, useUpdateUserMutation } from '../../../services'
+import { useState } from 'react'
 
 import s from './personal-information.module.scss'
-
-export type UpdateProfileType = ProfileFormInput & { avatar?: File }
 
 type PersonalInformationProps = {
   name: string
   email: string
   avatar: string
-  onChangeProfile: (profile: UpdateProfileType) => void
+  onSaveChanges: (value: string | undefined) => void
   onLogout: () => void
 }
 
 export const PersonalInformation = (props: PersonalInformationProps) => {
-  const { name, email, avatar, onChangeProfile, onLogout } = props
+  const { name, email, avatar, onLogout, onSaveChanges } = props
 
-  const { handleSubmit, control } = UseProfileForm()
+  const { handleSubmit, control } = UseProfileAvatarForm()
+  const { data: me } = useGetMeQuery()
+  const [updateAvatar] = useUpdateUserMutation()
 
   const [editMode, setEditMode] = useState(false)
-  const editModeOnHandler = () => setEditMode(true)
+  const activateEditMode = () => setEditMode(true)
 
-  const changeAvatarHandler = (avatar: File) => {
-    onChangeProfile({ avatar })
-  }
+  const onSubmit = handleSubmit((data: ProfileAvatarForm) => {
+    const form = new FormData()
 
-  const onSubmit = handleSubmit(data => {
-    onChangeProfile({ name: data.name })
-    setEditMode(false)
+    form.append('name', me?.name ?? '')
+    form.append('avatar', data.avatar ?? '')
+
+    updateAvatar(form)
   })
 
   return (
     <Card className={s.card}>
-      <Typography variant={'large'}>Personal Information</Typography>
-      <UserAvatar
-        className={s.avatar}
-        name={name}
-        src={avatar}
-        width={96}
-        height={96}
-        avatarContent={!editMode && <FileInput onChange={changeAvatarHandler} trigger={<Edit />} />}
-      />
-      {editMode ? (
-        <form onSubmit={onSubmit}>
-          <ControlledTextField
-            control={control}
-            name={'name'}
-            label={'Nickname'}
-            className={s.textField}
-          />
-          <Button type={'submit'} className={s.saveBtn} fullWidth>
-            Save Changes
-          </Button>
-        </form>
-      ) : (
+      <Typography variant={'large'} as={'h1'}>
+        Personal Information
+      </Typography>
+      <UserAvatar className={s.avatar} name={name} src={avatar} size={'6rem'} />
+      {!editMode ? (
         <>
+          <div className={s.editAvatar}>
+            <form onChange={onSubmit} className={s.formFileInput}>
+              <FileInput control={control} withPreview={false} name={'avatar'}>
+                {onClick => <Edit onClick={onClick} />}
+              </FileInput>
+            </form>
+          </div>
           <div className={s.name}>
             <Typography variant={'h1'}>{name}</Typography>
-            <Edit onClick={editModeOnHandler} className={s.editIcon} />
+            <Edit onClick={activateEditMode} className={s.editIcon} />
           </div>
           <Typography variant={'body_2'} className={s.email}>
             {email}
@@ -72,6 +64,14 @@ export const PersonalInformation = (props: PersonalInformationProps) => {
             Logout
           </Button>
         </>
+      ) : (
+        <EditableTextField
+          text={name}
+          onSaveChanges={value => {
+            onSaveChanges(value)
+            setEditMode(false)
+          }}
+        />
       )}
     </Card>
   )
