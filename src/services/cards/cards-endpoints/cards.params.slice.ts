a76d21c1e90e2f6371e2b3286dec3a44c89ca3cb
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-18 20:10:20 +0300

    feat: initialize Redux slice for cards query parameters

---
+++ src/services/cards/cards-endpoints/cards.params.slice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+import { GetCardsRequestType } from '../../packs/packs-endpoints/packs.types.ts'
+
+type initialStateType = {
+  queryParams: Partial<GetCardsRequestType>
+}
+
+const initialState: initialStateType = {
+  queryParams: {
+    id: '',
+    question: '',
+    answer: '',
+    orderBy: '',
+    currentPage: 1,
+    itemsPerPage: 10,
+  },
+}
+
+const slice = createSlice({
+  name: 'packsParams',
+  initialState,
+  reducers: {
+    setQueryParams: (state, action: PayloadAction<Partial<GetCardsRequestType>>) => {
+      state.queryParams = { ...state.queryParams, ...action.payload }
+    },
+  },
+})
+
+export const cardsReducer = slice.reducer
+export const cardsActions = slice.actions
