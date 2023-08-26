0fd36952d53d340a8e8ebd346f017f583b9d34ac
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-26 05:20:12 +0300

    feat: implementation profile API

---
+++ src/services/profile/profile-endpoints/profile.api.ts
@@ -0,0 +1,19 @@
+import { commonApi } from '../../common/base-query-with-reauth.ts'
+import { RequestDataUpdateMe, ResponseUpdateMe } from './profile.type.ts'
+
+export const profileApi = commonApi.injectEndpoints({
+  endpoints: builder => ({
+    updateUser: builder.mutation<ResponseUpdateMe, RequestDataUpdateMe>({
+      query: body => {
+        return {
+          method: 'PATCH',
+          url: 'auth/me',
+          body,
+        }
+      },
+      invalidatesTags: ['me'],
+    }),
+  }),
+})
+
+export const { useUpdateUserMutation } = profileApi
---
+++ src/services/profile/profile-endpoints/profile.type.ts
@@ -0,0 +1,11 @@
+export type ResponseUpdateMe = {
+  avatar: string
+  id: string
+  email: string
+  isEmailVerified: boolean
+  name: string
+  created: string
+  updated: string
+}
+
+export type RequestDataUpdateMe = FormData
