bddab9cd0105670af475882bf8c9f05e80e5b240
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-26 05:06:31 +0300

    feat: added components packs in services

---
+++ src/services/packs/components/packs.tsx
@@ -0,0 +1,10 @@
+import { PacksMain } from '../../../components/packs'
+import { Page } from '../../../components/ui'
+
+export const Packs = () => {
+  return (
+    <Page flex>
+      <PacksMain />
+    </Page>
+  )
+}
