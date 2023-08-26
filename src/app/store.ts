812f39df1b3c2bb2c90c0db8315400be9384703c
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-26 05:58:00 +0300

    upd: add reducers to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,9 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { commonApi } from '../services/common/base-query-with-reauth.ts'
+import { packsReducer } from '../services/packs/packs-endpoints/packs.params.slice.ts'
+import { cardsReducer } from '../services/cards/cards-endpoints/cards.params.slice.ts'
 
 export const store = configureStore({
   reducer: {
+    packsReducer,
+    cardsReducer,
     [commonApi.reducerPath]: commonApi.reducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(commonApi.middleware),
