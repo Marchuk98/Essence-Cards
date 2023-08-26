d864612b7469f452112b4e08ad796f2e155855bb
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-08-26 04:24:36 +0300

    feat: implementation of selectors for state packs

---
+++ src/services/packs/packs-endpoints/packs.params.selectors.ts
@@ -0,0 +1,13 @@
+import { RootState } from '../../../app/store.ts'
+
+export const selectPackName = (state: RootState) => state.packsReducer.queryParams.name
+export const selectPackAuthorId = (state: RootState) => state.packsReducer.queryParams.authorId
+export const selectPackOrderBy = (state: RootState) => state.packsReducer.queryParams.orderBy
+export const selectPackCurrentPage = (state: RootState) =>
+  state.packsReducer.queryParams.currentPage
+export const selectPackItemsPerPage = (state: RootState) =>
+  state.packsReducer.queryParams.itemsPerPage
+export const selectPackMinCardsCount = (state: RootState) =>
+  state.packsReducer.queryParams.minCardsCount
+export const selectPackMaxCardsCount = (state: RootState) =>
+  state.packsReducer.queryParams.maxCardsCount
