94e77a3dd12aa377b95ec05ed3b20b27fee8b40a
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-09-13 23:54:13 +0300

    feat: added answer component

---
+++ src/components/learn-pack/answer/answer.tsx
@@ -0,0 +1,58 @@
+import { RadioGroup, Typography } from '../../ui'
+import { BlurHashCanvas } from '../../ui/image'
+import { answersGrade } from './answersGrade.ts'
+import { ImageModal } from '../../modals-actions/image-modal'
+import { useImageOpen } from '../../../common/constants/useImageOpen.ts'
+import s from './answer.module.scss'
+
+type AnswerType = {
+  answer?: string
+  answerImg?: string
+  radioValue: string
+  setRadioValue: (value: string) => void
+}
+
+export const Answer = (props: AnswerType) => {
+  const { answer, answerImg, radioValue, setRadioValue } = props
+  const { openImageInModal, setImageModalOpen, image, isImageModalOpen } = useImageOpen()
+
+  return (
+    <div>
+      <ImageModal
+        src={image}
+        alt={'answer'}
+        isOpen={isImageModalOpen}
+        setIsOpen={setImageModalOpen}
+      />
+      <Typography as={'div'} variant={'body_1'} className={s.answer}>
+        <Typography as={'span'} variant={'subtitle_1'} className={s.titleAnswer}>
+          Answer:{' '}
+        </Typography>
+        {answer}
+        {answerImg && (
+          <div className={s.answerCover}>
+            <BlurHashCanvas
+              src={answerImg}
+              alt={'answer'}
+              blurWidth={362}
+              blurHeight={120}
+              className={s.img}
+              onClick={() => openImageInModal(answerImg)}
+            />
+          </div>
+        )}
+      </Typography>
+
+      <Typography variant={'subtitle_1'} className={s.feedback}>
+        Rate yourself:
+      </Typography>
+
+      <RadioGroup
+        options={answersGrade}
+        value={radioValue}
+        className={s.radioGroup}
+        onValueChange={setRadioValue}
+      />
+    </div>
+  )
+}
---
+++ src/components/learn-pack/answer/answersGrade.ts
@@ -0,0 +1,7 @@
+export const answersGrade: { value: string; label: string }[] = [
+  { value: '1', label: 'Did not know' },
+  { value: '2', label: 'Forgot' },
+  { value: '3', label: 'A lot of thought' },
+  { value: '4', label: 'Confused' },
+  { value: '5', label: 'Know the answer' },
+]
