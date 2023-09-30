891b14cd57facfc16ea6afa5e60dd6b7fc795c3d
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-01 00:10:47 +0300

    feat: implements cards page with cards main component

---
+++ src/services/cards/components/cards.tsx
@@ -0,0 +1,10 @@
+import { Page } from '../../../components/ui'
+import { CardsMain } from '../../../components/cards'
+
+export const Cards = () => {
+  return (
+    <Page flexTb>
+      <CardsMain />
+    </Page>
+  )
+}
