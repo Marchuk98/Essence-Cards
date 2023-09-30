4a9f42f1d15eacba0423d1b4554f1fb9f394e068
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-01 02:52:34 +0300

    upd: enhanced learn component with data fetching and integration

--- src/services/learn/components/learn.tsx
+++ src/services/learn/components/learn.tsx
@@ -1,4 +1,40 @@
+import { GradeType, Page } from '../../../components/ui'
+import { LearnPack } from '../../../components/learn-pack'
+import { useParams } from 'react-router-dom'
+import {
+  useGetPackQuery,
+  useLearnPackQuery,
+  useUpdateCardGradeMutation,
+} from '../../packs/packs-endpoints/packs.api.ts'
+import { MainLoader } from '../../../assets/loaders/main-loader'
+
 export const Learn = () => {
-  return <div>Learn</div>
+  const { id } = useParams()
+  const { data: pack, isLoading } = useGetPackQuery({ id: id || '' })
+  const [updateGrade] = useUpdateCardGradeMutation()
+  const { data: randomCard } = useLearnPackQuery({ id: id || '' })
+
+  return (
+    <Page flexTb>
+      {isLoading && <MainLoader />}
+      {pack && randomCard && (
+        <LearnPack
+          packName={pack.name}
+          numberEfforts={randomCard.shots}
+          question={randomCard.question}
+          questionImg={randomCard.questionImg}
+          answer={randomCard.answer}
+          answerImg={randomCard.answerImg}
+          loadNextQuestion={data =>
+            updateGrade({
+              id: pack.id,
+              grade: +data as GradeType,
+              cardId: randomCard.id,
+            })
+          }
+        />
+      )}
+    </Page>
+  )
 }
 
