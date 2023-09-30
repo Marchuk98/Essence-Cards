bbc12991eae742757999bf9f00cc0feddbfe56ab
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-01 01:05:57 +0300

    upd: added display of stubs when loading data

--- src/components/packs/packs-table/pack-table-body.tsx
+++ src/components/packs/packs-table/pack-table-body.tsx
@@ -1,54 +1,82 @@
 import { Table, Typography } from '../../ui'
 import { PATH } from '../../../common'
-import { TableActionButton } from '../../ui/table-action-button'
-import { usePacks } from '../../../services/packs/hooks/usePacks.ts'
-import { ItemType } from '../../../services/packs/packs-endpoints/packs.types.ts'
-import icons from '../../../assets/images/react.png'
+import { PackType } from '../../../services/packs/packs-endpoints/packs.types.ts'
+import packImg from '../../../assets/images/react.png'
+import { useNavigate } from 'react-router-dom'
+import { Skeleton } from '../../ui/skeleton'
+import { PacksTableActions } from '../packs-table-actions'
 
 import s from './pack-table-body.module.scss'
 
 type PackTableBodyType = {
-  packContent: ItemType[]
+  packContent: PackType[]
+  isMe: string
+  isFetching: boolean
 }
 
-export const PackTableBody = ({ packContent }: PackTableBodyType) => {
-  const { isMe } = usePacks()
+export const PackTableBody = ({ packContent, isMe, isFetching }: PackTableBodyType) => {
+  const navigate = useNavigate()
 
   return (
     <Table.Body>
       {packContent.map(item => (
         <Table.Row key={item.id}>
           <Table.TableCell>
-            <Typography
-              className={s.cellContent}
-              variant={'link_1'}
-              as={'a'}
-              href={`${PATH.CARDS}/${item.id}`}
-            >
-              {item.cover === null ? (
-                <img src={icons} alt={'default icons name'} />
+            <div className={s.cellContent} onClick={() => navigate(`${PATH.CARDS}/${item.id}`)}>
+              {isFetching ? (
+                <>
+                  <Skeleton width={120} height={48} />
+                  <Skeleton width={60} height={24} />
+                </>
               ) : (
-                <img className={s.cellImgContent} src={item.cover} alt={'icons for name'} />
+                <>
+                  {item.cover === null ? (
+                    <img src={packImg} alt={'default icons name'} />
+                  ) : (
+                    <img className={s.cellImgContent} src={item.cover} alt={'icons for name'} />
+                  )}
+                  {item.name}
+                </>
               )}
-              {item.name}
-            </Typography>
+            </div>
           </Table.TableCell>
           <Table.TableCell>
-            <Typography variant={'body_2'}>{item.cardsCount}</Typography>
+            {isFetching ? (
+              <Skeleton width={24} height={24} />
+            ) : (
+              <Typography variant={'body_2'}>{item.cardsCount}</Typography>
+            )}
           </Table.TableCell>
           <Table.TableCell>
-            <Typography variant={'body_2'}>
-              {new Date(item.updated).toLocaleString('ru')}
-            </Typography>
+            {isFetching ? (
+              <Skeleton width={100} height={24} />
+            ) : (
+              <Typography variant={'body_2'}>
+                {new Date(item.updated).toLocaleString('ru')}
+              </Typography>
+            )}
           </Table.TableCell>
           <Table.TableCell>
-            <Typography variant={'body_2'}>{item.author.name}</Typography>
+            {isFetching ? (
+              <Skeleton width={100} height={24} />
+            ) : (
+              <Typography variant={'body_2'}>{item.author.name}</Typography>
+            )}
           </Table.TableCell>
           <Table.TableCell>
-            <TableActionButton
-              isActiveActions={item.userId === isMe}
-              pack={{ id: item.id, name: item.name, isPrivate: item.isPrivate, cover: item.cover }}
-            />
+            {isFetching ? (
+              <Skeleton width={70} height={24} />
+            ) : (
+              <PacksTableActions
+                isActiveActions={item.userId === isMe}
+                pack={{
+                  id: item.id,
+                  name: item.name,
+                  isPrivate: item.isPrivate,
+                  cover: item.cover,
+                }}
+              />
+            )}
           </Table.TableCell>
         </Table.Row>
       ))}
