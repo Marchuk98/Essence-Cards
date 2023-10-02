da25ed6b70efdbbaa4bc0a1107bc81cb6360bcc5
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-10-03 00:02:28 +0300

    upd: refactoring and adding properties for dark/light mode control

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
 import { Layout } from '../components/layout'
+import { ThemeToogle } from '../components/ui/theme-toogle'
 
 export function App() {
   return (
     <>
+      <ThemeToogle />
       <Layout />
     </>
   )
--- src/assets/icons/ArrowLeftIcon.tsx
+++ src/assets/icons/ArrowLeftIcon.tsx
@@ -4,7 +4,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="none" {...props}>
     <g>
       <path
-        fill="#fff"
+        fill="var(--color-light-100)"
         d="M12.7 7.3h-8l2.5-2.9a.7.7 0 0 0-1-.8l-3.4 4v.2l-.1.2V8.3l.1.1 3.4 4a.7.7 0 0 0 1-.8l-2.4-3h7.9a.7.7 0 1 0 0-1.3Z"
       />
     </g>
